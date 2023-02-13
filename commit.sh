ccb796bce7162347aade58db6835a21d51b62ee9
Michael Asare <nacolepsy@gmail.com>
2023-02-13 20:44:11 +0000

    .gitignore to work

--- commit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git add .
-read -p 'commit message: ' message
-git commit -m "$message"
-git push
--- file.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-read -p "File name: " name
-echo "#!/bin/bash" > $name
-chmod u+x $name
-vim $name
